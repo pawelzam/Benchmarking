9f6892d509e111e3d839fda9e37fd9dcdf7a70b0
Pawel Zamrij <PZAM@danskebank.dk>
2024-10-04 15:41:10 +0200

    Dealing with dates

--- FastOperations/Program.cs
+++ FastOperations/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using FastOperations.Scenarios;
 
-BenchmarkRunner.Run<Tasks>();
+//BenchmarkRunner.Run<Tasks>();
+//BenchmarkRunner.Run<Dates>();
 
 // await new Exceptions().HandleAsync();
-
---
+++ FastOperations/Scenarios/Dates.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace FastOperations.Scenarios;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class Dates
+{
+    private const string DateAsText = "2024-10-04";
+
+    [Benchmark]
+    public DateTime ConvertUsingSubstring()
+    {
+        var day = int.Parse(DateAsText.Substring(8, 2));
+        var month = int.Parse(DateAsText.Substring(5, 2));
+        var year = int.Parse(DateAsText[..4]);
+
+        return new DateTime(year, month, day);
+    }
+
+    [Benchmark]
+    public DateTime ConvertUsingSlice()
+    {
+        ReadOnlySpan<char> span = DateAsText;
+        var day = int.Parse(span.Slice(8,2));
+        var month = int.Parse(span.Slice(5,2));
+        var year = int.Parse(span.Slice(0,4));
+
+        return new DateTime(year, month, day);
+    }
+}
